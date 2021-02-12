b8a6cbe3c86147ae2dcd13a56f21e211881d4c63
Carla <carladaianasl@gmail.com>
2021-02-12 10:12:18 -0300

    icons in the info icon

--- src/components/Navbar/Menuinfoicon.js
+++ src/components/Navbar/Menuinfoicon.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import EmojiTransportationOutlinedIcon from '@material-ui/icons/EmojiTransportationOutlined';
+import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
+import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
 
 function SimpleMenuInfo() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -14,6 +19,12 @@ function SimpleMenuInfo() {
     setAnchorEl(null);
   };
 
+  const useStyles = makeStyles({
+    root: {
+      backgroundColor: 'red',
+    },
+  });
+
   return (
     <div>
       <InfoOutlinedIcon style={{ fontSize: 28 }} className="icon-info" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}></InfoOutlinedIcon>
@@ -24,9 +35,11 @@ function SimpleMenuInfo() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem onClick={handleClose}> <EmojiTransportationOutlinedIcon /> Drift sidecorp</MenuItem>
+        <MenuItem onClick={handleClose}> <AccountCircleOutlinedIcon /> 1 Position</MenuItem>
+        <MenuItem onClick={handleClose}> <MonetizationOnOutlinedIcon /> $CAD 100 - 140k</MenuItem>
+        <MenuItem onClick={handleClose}> <RoomOutlinedIcon /> Remote</MenuItem>
+        <MenuItem onClick={handleClose}> <i class="fas fa-laptop-code"></i> Remote</MenuItem>
       </Menu>
     </div>
   );

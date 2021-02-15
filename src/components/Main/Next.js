73d6bcc6c7c6c5fad4f859675ad040e0a097a913
Carla <carladaianasl@gmail.com>
2021-02-15 00:54:47 -0300

    adjusting API integration in next

--- src/components/Main/Next.js
+++ src/components/Main/Next.js
@@ -1,57 +1,76 @@
 import React, { Component } from 'react';
 import Avatars from "./avatars";
 import Candidates from "./candidates-line";
+import Linestyle from "../lineHR";
 import './body.css';
 
 
-function Calendar() {
-  return (
-  	<div>
-	    <div className="grid-container top">
-		  <div className="grid-item first">Candidate</div>
-		  <div className="grid-item">Interview Step</div>
-		  <div className="grid-item">Scheduled time</div>
-		  <div className="grid-item"></div>
-		  <div className="grid-item"></div>
-		  <div className="grid-item"></div>
-		</div>
+class Next extends Component {
 
-		<hr />
+	constructor(props){
+		super(props);
+		this.state = {
+			items: [],
+			isLoaded: false,
+		}
+	}
 
-		<Candidates 
-			style="grid-container"
-		  	img="/static/images/avatar/2.jpg"
-		  	candidate="Brooklyn Simmons"
-		  	qualification="FullStack Developer"
-		  	interviewStep="Technical Interview"
-		  	textSchedule="Tue, Nov, 18th - 10:00am"
-		  	textButton="Get interview link"
-		/>
+	componentDidMount(){
 
-		<Candidates 
-			style="grid-container"
-		  	img="/static/images/avatar/2.jpg"
-		  	candidate="Esther Howard"
-		  	qualification="FullStack Developer"
-		  	interviewStep="Technical Interview"
-		  	textSchedule="Tue, Nov, 18th - 10:00am"
-		  	textButton="Get interview link"
-		/>
+		fetch('https://api-test.vanhack.dev/Calendar')
+			.then(res => res.json())
+			.then(json => {
 
-		<Candidates 
-			style="grid-container"
-		  	img="/static/images/avatar/2.jpg"
-		  	candidate="Esther Howard"
-		  	qualification="FullStack Developer"
-		  	interviewStep="Technical Interview"
-		  	textSchedule="Tue, Nov, 18th - 10:00am"
-		  	textButton="Get interview link"
-		/>
-		
-		
-	</div>
-  );
+				this.setState({
+					isLoaded: true,
+					items: json, 
+				})
+			});
+	}
+
+	render(){
+
+		var { isLoaded, items } = this.state;
+
+		if (!isLoaded){
+			return <div> Loading .. </div>;
+		}
+
+		else {
+
+		  	return (
+			  	<div>
+				    <div className="grid-container top">
+					  <div className="grid-item first">Candidate</div>
+					  <div className="grid-item">Interview Step</div>
+					  <div className="grid-item">Scheduled time</div>
+					  <div className="grid-item"></div>
+					  <div className="grid-item"></div>
+					  <div className="grid-item"></div>
+					</div>
+
+					<Linestyle color="#f7f7f7" />
+
+					
+							{items.map(item => (
+								<div key={item.id}>
+									<Candidates 
+										style="grid-container"
+						  				img={item.image}
+						  				candidate={item.name}
+						  				qualification={item.title}
+						  				interviewStep={item.interviewStep}
+						  				textSchedule={item.scheduledTime}
+						  				textButton="Get interview link"
+									/>
+								</div>
+							))}
+					
+				</div>
+  			);
+		}
+	}
 }
 
 
-export default Calendar;
+export default Next;

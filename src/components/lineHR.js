5056bf9b75d7f1fdcaf1a7e0fd3a3c290a602c6d
Carla <carladaianasl@gmail.com>
2021-02-15 00:33:08 -0300

    adjusting API integration in calendar

---
+++ src/components/lineHR.js
@@ -0,0 +1,13 @@
+const Linestyle = ({ color }) => (
+    <hr
+        style={{
+            backgroundColor: color,
+            marginLeft: '1%',
+            width: '98%',
+            textDecoration: "none",
+            border: '0px',
+        }}
+    />
+);
+
+export default Linestyle;
